469cf871239979cc1913dbb21455f9d47a8cb851
everestwinter <EverestWinter@gmail.com>
2011-06-19 15:56:09 +0000

    反射工具类

---
+++ mobile-canteen-manager/src/main/java/com/sjct/common/util/ReflectionUtils.java
@@ -0,0 +1,59 @@
+package com.sjct.common.util;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReflectionUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(ReflectionUtils.class);
+	
+	/**
+	 * 通过反射,获得Class定义中声明的父类的泛型参数的类型. eg. public UserDao extends
+	 * HibernateDao<User>
+	 * 
+	 * @param clazz
+	 *            The class to introspect
+	 * @return the first generic declaration, or Object.class if cannot be
+	 *         determined
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> Class<T> getSuperClassGenricType(final Class clazz) {
+		return getSuperClassGenricType(clazz, 0);
+	}
+
+	/**
+	 * 通过反射,获得定义Class时声明的父类的泛型参数的类型.
+     */
+	@SuppressWarnings("unchecked")
+	public static Class getSuperClassGenricType(final Class clazz,
+			final int index) {
+
+		Type genType = clazz.getGenericSuperclass();
+
+		if (!(genType instanceof ParameterizedType)) {
+			logger.warn(clazz.getSimpleName()
+					+ "'s superclass not ParameterizedType");
+			return Object.class;
+		}
+
+		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+
+		if (index >= params.length || index < 0) {
+			logger.warn("Index: " + index + ", Size of "
+					+ clazz.getSimpleName() + "'s Parameterized Type: "
+					+ params.length);
+			return Object.class;
+		}
+		if (!(params[index] instanceof Class)) {
+			logger
+					.warn(clazz.getSimpleName()
+							+ " not set the actual class on superclass generic parameter");
+			return Object.class;
+		}
+		return (Class) params[index];
+	}
+
+}
